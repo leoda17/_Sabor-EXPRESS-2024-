db44ca065e00e637a1f771d79d834a9d5eb764f2
lele <140726302+leoda17@users.noreply.github.com>
2024-08-23 07:43:10 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+print('=== Laços Pet ===\n')
+
+print('1. Cadastrar pet')
+print('2. Listar pets')
+print('3. Adotar pet')
+print('4. Verificar estoque de produtos')
+print('5. Sair\n')
+
+opcao_escolhida = input("Escolha uma opção:")
+print("Você escolheu a opção: ",opcao_escolhida)
+print("ativar")
