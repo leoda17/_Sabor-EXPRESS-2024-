5942dbf7a52830e0ca05a1824de72ee7846b26f8
lele <140726302+leoda17@users.noreply.github.com>
2024-08-27 11:06:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-print('=== Laços Pet ===\n')
+import os
 
-print('1. Cadastrar pet')
-print('2. Listar pets')
-print('3. Adotar pet')
-print('4. Verificar estoque de produtos')
-print('5. Sair\n')
+def exibir_nome_do_programa():
+    print("""
+███████╗██╗░░░░░█████╗░░██████╗░██████╗░  ██████╗░███████╗████████╗
+██╔════╝██║░░░░██╔══██╗██╔════╝██╔══██╗  ██╔══██╗██╔════╝╚══██╔══╝
+█████╗░░██║░░░██║░░██║╚█████╗░██████╔╝  ██████╔╝█████╗░░░░░██║░░░
+██╔══╝░░██║░░░██║░░██║░╚═══██╗██╔══██╗  ██╔═══╝░██╔══╝░░░░░██║░░░
+███████╗███████╗╚█████╔╝██████╔╝██║░░██║  ██║░░░░░███████╗░░░██║░░░
+╚══════╝╚══════╝░╚════╝░╚═════╝░╚═╝░░╚═╝  ╚═╝░░░░░╚══════╝░░░╚═╝░░░
+""")
 
-opcao_escolhida = input("Escolha uma opção:")
-print("Você escolheu a opção: ",opcao_escolhida)
-print("ativar")
+def exibir_opcoes():
+    print('1. Cadastrar animal')
+    print('2. Listar animais')
+    print('3. Consultar animal')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o sistema de clínica veterinária\n')
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    
+    if opcao_escolhida == 1:
+        print('Cadastrar animal')
+    elif opcao_escolhida == 2:
+        print('Listar animais')
+    elif opcao_escolhida == 3:
+        print('Consultar animal')
+    elif opcao_escolhida == 4:
+        finalizar_app()
+    else:
+        opcao_invalida()
+
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ =='__main__':
+    main()
